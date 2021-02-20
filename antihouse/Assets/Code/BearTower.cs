ae00f32879d454d80e681c02f40cca8a07a33ec0
ritaeperjesi <eperjesirit@gmail.com>
2021-02-20 23:21:16 +0100

    unity file
    unity file

---
+++ antihouse/Assets/Code/BearTower.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BearTower : MonoBehaviour
+{
+    public GameObject maci;
+    public int width = 10;
+    public int height = 4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        // Instantiate at position (0, 0, 0) and zero rotation.
+
+
+        for (int y = -5; y < height; ++y)
+        {
+            for (int x = -10; x < width; ++x)
+            {
+                Instantiate(maci, new Vector3((x+0.5f), y+1, 0), Quaternion.identity);
+            }
+        }
+
+        for (int y = -5; y < height; ++y)
+        {
+            for (int x = -10; x < width; ++x)
+            {
+                Instantiate(maci, new Vector3((x + 0.5f), y + 1, 14), Quaternion.identity);
+            }
+        }
+
+     
+
+        /* for (int y = -2; y<height; ++y)
+           {
+               for (int x = 1; x<width; ++x)
+               {
+                   Instantiate(maci, new Vector3(-x, y+1, 0), Quaternion.identity);
+               }
+           } */
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ antihouse/Assets/Code/ceilingGrow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ceilingGrow : MonoBehaviour
+{
+   // public GameObject ceiling;
+  //  public Vector3 ceilingPosition = new Vector3(0, 0, 0);
+  //  public float ceilingSpeed = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Changes the position to x:1, y:1, z:0
+        transform.position = new Vector3(5, 8, 0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // myMoveUp();
+        // We add +1 to the x axis every frame.
+        // Time.deltaTime is the time it took to complete the last frame
+        // The result of this is that the object moves one unit on the x axis every second
+        transform.position += new Vector3(0,  1 * Time.deltaTime,0);
+    }
+
+  /*  void myMoveUp()
+    {
+        ceilingPosition = GetComponent<Transform>().position;
+        ceilingPosition.z += ceilingSpeed * Time.deltaTime;
+        Instantiate(ceiling, ceilingPosition, Quaternion.identity);
+    } */
+}
